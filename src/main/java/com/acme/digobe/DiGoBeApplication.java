a03dbf10eae5fff8cfd68a8b3580d69328d3a38d
Italo Silva <italo.silva.silvera@outlook.com>
2022-06-26 22:45:09 -0500

    initial commit

---
+++ src/main/java/com/acme/digobe/DiGoBeApplication.java
@@ -0,0 +1,13 @@
+package com.acme.digobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiGoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiGoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/digobe/DiGoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.digobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiGoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
